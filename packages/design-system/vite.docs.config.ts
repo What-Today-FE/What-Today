39bde702bb143cdb73f76d0c7fcc121fed6c2ad5
MyungJiwoo <1206jiwoo@gmail.com>
2025-07-10 17:02:18 +0900

    ♻️ [#2] Refactor: 서비스와 디자인 시스템 완전 분리 & 디자인 시스템의 라이브러리 빌드 모드 추가

--- packages/design-system/vite.docs.config.ts
+++ packages/design-system/vite.docs.config.ts
@@ -1,24 +1,11 @@
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/index.ts'),
-      name: 'DesignSystem',
-      fileName: 'index',
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    outDir: 'dist/components',
+    outDir: 'dist/docs',
     emptyOutDir: true,
   },
   resolve: {
