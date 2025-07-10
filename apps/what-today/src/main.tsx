af5ec87e886ca560478ad4577e6f279155fdb3f7
HarrySeop <harryseop@gmail.com>
2025-07-10 13:50:14 +0900

    📦 [#14] Chore: 쿼리 프로바이더 생성

--- apps/what-today/src/main.tsx
+++ apps/what-today/src/main.tsx
@@ -1,13 +1,16 @@
 import './index.css';
 
+import QueryProvider from '@libs/queryProvider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
-import router from './routes/index.tsx';
+import router from './routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryProvider>
+      <RouterProvider router={router} />
+    </QueryProvider>
   </StrictMode>,
 );
