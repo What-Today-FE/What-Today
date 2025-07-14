10879a760c5b443ccfe3cda3f5110deb027ad451
MyungJiwoo <1206jiwoo@gmail.com>
2025-07-14 14:27:39 +0900

    ✨ [#37] Feat: Header에 아이콘 / 프로필 이미지 추가

--- apps/what-today/src/components/Header.tsx
+++ apps/what-today/src/components/Header.tsx
@@ -1,3 +1,6 @@
+import { BellIcon, DotIcon, ProfileLogo } from '@what-today/design-system';
+import { ImageLogo } from '@what-today/design-system';
+import { TextLogo } from '@what-today/design-system';
 import { Link } from 'react-router-dom';
 
 import { useWhatTodayStore } from '@/stores';
@@ -6,33 +9,43 @@ export default function Header() {
   const { user, isLoggedIn } = useWhatTodayStore();
 
   return (
-    <div className='flex w-full justify-between px-[10vw] py-16'>
-      <div className='flex gap-8'>
-        <div className='bg-primary-100 size-24 rounded-full' />
-        <div className='hidden sm:block'>오늘뭐해</div>
-      </div>
+    <header className='relative z-50 flex w-full justify-between py-16'>
+      <Link className='flex items-center gap-8' to='/'>
+        <ImageLogo className='size-24' />
+        <TextLogo className='hidden h-fit w-65 sm:block' />
+      </Link>
 
       {isLoggedIn ? (
-        <div className='text-md flex items-center gap-8'>
-          <div className='cursor-pointer hover:opacity-60'>🔔</div>
+        <div className='text-md flex items-center gap-8 text-gray-950'>
+          <div className='relative cursor-pointer hover:opacity-60'>
+            {/* 알람 있을 때 사용 */}
+            <DotIcon className='absolute top-2 left-12 size-8' color='var(--color-red-500)' />
+            <BellIcon className='size-20' color='var(--color-gray-600)' />
+          </div>
 
           <div className='mx-12 h-16 w-px bg-gray-100' />
 
-          <Link className='flex items-center gap-12 hover:opacity-60' to='/mypage'>
-            <div className='bg-primary-100 size-24 rounded-full' />
-            <p>{user?.nickname} 님</p>
+          <Link className='flex items-center gap-8 hover:opacity-60' to='/mypage'>
+            {user?.profileImageUrl ? (
+              <img alt='프로필 이미지' className='size-24 rounded-full object-cover' src={user.profileImageUrl} />
+            ) : (
+              <ProfileLogo className='size-24' />
+            )}
+
+            <p>{user?.nickname}</p>
           </Link>
         </div>
       ) : (
-        <div className='text-md flex items-center gap-24'>
+        <div className='text-md flex items-center gap-4 sm:gap-12'>
           <Link to='/login'>
             <p className='cursor-pointer hover:opacity-60'>로그인</p>
           </Link>
+          <div className='mx-12 h-12 w-px bg-gray-100' />
           <Link to='/signup'>
             <p className='cursor-pointer hover:opacity-60'>회원가입</p>
           </Link>
         </div>
       )}
-    </div>
+    </header>
   );
 }
