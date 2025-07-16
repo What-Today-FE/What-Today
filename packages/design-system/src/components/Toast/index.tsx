6ea48968dceb49fdf9cdd986ce589c3e378238c5
HarrySeop <harryseop@gmail.com>
2025-07-17 03:17:26 +0900

    ♻️ [#54] Refactor: 분리한 파일들로 Toast 수정

--- packages/design-system/src/components/Toast/index.tsx
+++ packages/design-system/src/components/Toast/index.tsx
@@ -1,6 +1,10 @@
 import { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
+import ToastClose from './ToastClose';
+import ToastDescription from './ToastDescription';
+import ToastTitle from './ToastTitle';
+
 export const useToast = () => {
   const toastRoot = useRef<ReactDOM.Root | null>(null);
   const ToastTimeout = useRef<NodeJS.Timeout | null>(null);
@@ -15,9 +19,12 @@ export const useToast = () => {
 
     toastRoot.current = ReactDOM.createRoot(node);
     toastRoot.current.render(
-      <div className='fixed top-4 right-4 z-50 w-300 rounded bg-black p-4 text-white shadow'>
-        <div className='font-bold'>{title}</div>
-        <div className='mt-1 text-sm'>{description}</div>
+      <div className='fixed top-4 right-4 z-50 flex w-300 items-start gap-3 rounded bg-black p-10 text-white shadow'>
+        <div className='flex-1'>
+          <ToastTitle>{title}</ToastTitle>
+          <ToastDescription>{description}</ToastDescription>
+        </div>
+        <ToastClose onClose={() => toastRoot.current?.unmount()} />
       </div>,
     );
 
