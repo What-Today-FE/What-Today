1028482b281f7c2ea1df28956dda1aa906cc7c71
Jihyun <wlguskim21@naver.com>
2025-07-19 15:20:17 +0900

    ♻️ [#72] Refactor: 사이드바, 레이아웃 반응형 구조 변경

--- apps/what-today/src/components/MypageSidebar.tsx
+++ apps/what-today/src/components/MypageSidebar.tsx
@@ -9,6 +9,7 @@ import {
   UserIcon,
 } from '@what-today/design-system';
 import { Link, useLocation } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 interface MypageSidebarProps {
   /**
@@ -16,12 +17,18 @@ interface MypageSidebarProps {
    * 전달되지 않을 경우 기본 아이콘(ProfileLogo)이 표시됩니다.
    */
   profileImgUrl?: string;
-
   /**
    * 로그아웃 버튼 클릭 시 실행되는 콜백 함수(아마 모달을 띄우지 않을까 싶습니다.)
    */
   onLogoutClick: () => void;
+  /**
+   * 클릭했을 때 sidebar 열림 여부 변경
+   */
   onClick: () => void;
+  /**
+   * MypageSidebar의 열림 여부
+   */
+  isOpen: boolean;
 }
 
 /**
@@ -38,7 +45,7 @@ interface MypageSidebarProps {
  *   onClick={() => logout()}
  * />
  */
-export default function MypageSidebar({ profileImgUrl, onLogoutClick, onClick }: MypageSidebarProps) {
+export default function MypageSidebar({ profileImgUrl, onLogoutClick, onClick, isOpen }: MypageSidebarProps) {
   const location = useLocation();
   /**
    * 사이드바에 표시할 고정 메뉴 항목 목록
@@ -52,34 +59,51 @@ export default function MypageSidebar({ profileImgUrl, onLogoutClick, onClick }:
   ];
 
   return (
-    <nav className='flex h-474 flex-col items-center gap-24 rounded-xl border border-gray-50 bg-white px-14 pt-24 pb-12 shadow-[0px_4px_24px_rgba(156,180,202,0.2)] md:w-180 xl:w-280'>
-      {profileImgUrl ? (
-        <img alt='프로필 이미지' className='bg-primary-100 size-120 rounded-full' src={profileImgUrl} />
-      ) : (
-        <ProfileLogo className='rounded-full' size={120} />
+    <nav
+      className={twMerge(
+        // 공통 스타일
+        'fixed z-50 w-180 rounded-xl border border-gray-50 bg-white shadow-[0px_4px_24px_rgba(156,180,202,0.2)] transition duration-300 md:static md:h-474 xl:w-280',
+        // 모바일에서 Drawer 위치
+        isOpen ? 'h-474 translate-x-0' : 'bg-primary-100 border-primary-100 h-50 -translate-x-full',
+        'md:translate-x-0',
       )}
-      <ul className='flex w-full flex-col justify-center'>
-        {items.map(({ label, icon: Icon, to }) => {
-          const isSelected = location.pathname.startsWith(to);
-          const baseClass = 'flex w-full cursor-pointer items-center gap-8 rounded-2xl px-20 py-14 text-gray-600';
-          const selectedClass = isSelected && 'bg-primary-100 text-gray-950';
-          const colorProps = isSelected ? '#3d9ef2' : '#707177';
+    >
+      {/* 콘텐츠: PC는 항상, 모바일은 isOpen일 때만 */}
+      <div
+        className={twMerge(
+          'flex h-full flex-col items-center gap-24 rounded-xl px-14 pt-24 pb-12',
+          isOpen ? 'flex' : 'hidden',
+          'md:flex',
+        )}
+      >
+        {profileImgUrl ? (
+          <img alt='프로필 이미지' className='bg-primary-100 size-120 rounded-full' src={profileImgUrl} />
+        ) : (
+          <ProfileLogo className='rounded-full' size={120} />
+        )}
+        <ul className='flex w-full flex-col justify-center'>
+          {items.map(({ label, icon: Icon, to }) => {
+            const isSelected = location.pathname.startsWith(to);
+            const baseClass = 'flex w-full cursor-pointer items-center gap-8 rounded-2xl px-20 py-14 text-gray-600';
+            const selectedClass = isSelected && 'bg-primary-100 text-gray-950';
+            const colorProps = isSelected ? '#3d9ef2' : '#707177';
 
-          return (
-            <li key={label}>
-              <Link className={`${baseClass} ${selectedClass} `} to={to} onClick={onClick}>
-                <div className='flex size-24 items-center justify-center'>
-                  <Icon color={`${colorProps}`} />
-                </div>
-                <div className='text-lg font-medium'>{label}</div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <Button className='w-full text-gray-400' size='lg' variant='ghost' onClick={onLogoutClick}>
-        <ExitIcon /> 로그아웃
-      </Button>
+            return (
+              <li key={label}>
+                <Link className={`${baseClass} ${selectedClass} `} to={to} onClick={onClick}>
+                  <div className='flex size-24 items-center justify-center'>
+                    <Icon color={`${colorProps}`} />
+                  </div>
+                  <div className='text-lg font-medium'>{label}</div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        <Button className='w-full text-gray-400' size='lg' variant='ghost' onClick={onLogoutClick}>
+          <ExitIcon /> 로그아웃
+        </Button>
+      </div>
     </nav>
   );
 }
--- apps/what-today/src/layouts/Mypage.tsx
+++ apps/what-today/src/layouts/Mypage.tsx
@@ -1,7 +1,6 @@
 import { Button, ChevronIcon } from '@what-today/design-system';
 import { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import { twMerge } from 'tailwind-merge';
 
 import MypageSidebar from '@/components/MypageSidebar';
 
@@ -19,15 +18,11 @@ export default function MyPageLayout() {
       {isSidebarOpen && (
         <div className='fixed inset-0 z-50 bg-black/30 md:hidden' onClick={() => setSidebarOpen(false)} />
       )}
-      {/* 사이드바 */}
-      <aside
-        className={twMerge(
-          'fixed z-100 rounded-xl bg-white shadow transition-transform duration-500 md:static md:translate-x-0 md:shadow-none',
-          isSidebarOpen ? 'translate-x-0' : '-translate-x-full',
-        )}
-      >
-        <MypageSidebar onClick={() => setSidebarOpen(false)} onLogoutClick={() => alert('hi')} />
-      </aside>
+      <MypageSidebar
+        isOpen={isSidebarOpen}
+        onClick={() => setSidebarOpen((prev) => !prev)}
+        onLogoutClick={() => alert('hi')}
+      />
       {/* Outlet으로 상세 화면 표시 */}
       <div className='flex-1 p-4'>
         <Button className='w-fit p-0 md:hidden' size='xs' variant='none' onClick={() => setSidebarOpen(true)}>
