e8668f45ea7dcc774def13b815dd54b80be762f2
Jihyun <wlguskim21@naver.com>
2025-07-20 21:37:07 +0900

    ♻️ [#80] Refactor: 캘린더에 onDateChange 전달할 수 있도록 prop추가

--- apps/what-today/src/components/reservations-status/ReservationCalendar.tsx
+++ apps/what-today/src/components/reservations-status/ReservationCalendar.tsx
@@ -2,11 +2,15 @@ import { Calendar } from '@what-today/design-system';
 import type { ReservationStatus } from 'node_modules/@what-today/design-system/dist/types/calendar/DayCell';
 interface ReservationCalendarProps {
   reservationsByDate: Record<string, Record<ReservationStatus, number>>;
+  onChange: (date: string) => void;
 }
-export default function ReservationCalendar({ reservationsByDate }: ReservationCalendarProps) {
+export default function ReservationCalendar({ reservationsByDate, onChange }: ReservationCalendarProps) {
   return (
     <div>
-      <Calendar.Root className='gap-8 rounded-3xl pt-20 pb-10 md:gap-30 md:shadow-[0px_4px_24px_rgba(156,180,202,0.2)]'>
+      <Calendar.Root
+        className='gap-8 rounded-3xl pt-20 pb-10 md:gap-30 md:shadow-[0px_4px_24px_rgba(156,180,202,0.2)]'
+        onDateChange={onChange}
+      >
         <Calendar.Header headerClass='py-6' titleClass='md:text-xl' />
         <Calendar.Grid divider weekdayClass='text-sm font-bold md:text-lg' weekdayType='long'>
           {(day) => {
