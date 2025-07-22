1272701c99f4b574f82117ddae1f44402936db1c
MyungJiwoo <1206jiwoo@gmail.com>
2025-07-22 17:41:17 +0900

    ✨ [#101] Feat: DefaultLayout에 Footer 추가

--- apps/what-today/src/layouts/DefaultLayout.tsx
+++ apps/what-today/src/layouts/DefaultLayout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@what-today/design-system';
 import { Outlet } from 'react-router-dom';
 
 import Header from '@/components/Header';
@@ -8,13 +9,10 @@ export default function DefaultLayout() {
       <header className='w-full max-w-7xl px-[5vw]'>
         <Header />
       </header>
-      {/* @Taeil08 Footer 추가하실 때 min-h만 수정 부탁드려요! */}
       <main className='min-h-[90vh] w-full max-w-6xl px-[5vw] xl:max-w-5xl'>
         <Outlet />
       </main>
-      <div className='w-full border-t-1 border-gray-50'>
-        <footer className='w-full max-w-7xl px-[5vw]'>{/* <Footer /> */}</footer>
-      </div>
+      <Footer />
     </div>
   );
 }
