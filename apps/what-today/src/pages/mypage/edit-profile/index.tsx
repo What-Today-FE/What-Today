e3bc461bcd555d05aaa1b8cf70e12007543762c2
MyungJiwoo <1206jiwoo@gmail.com>
2025-07-22 18:20:00 +0900

    ✨ [#101] Feat: 뒤로가기 버튼 추가 & 비밀번호 수정할 때만 비밀번호 확인 Input이 보이도록 수정

--- apps/what-today/src/pages/mypage/edit-profile/index.tsx
+++ apps/what-today/src/pages/mypage/edit-profile/index.tsx
@@ -1,28 +1,36 @@
-import { Button, ProfileImageInput } from '@what-today/design-system';
+import { Button, ChevronIcon, ProfileImageInput } from '@what-today/design-system';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import EmailInput from '@/components/signup/EmailInput';
 import NicknameInput from '@/components/signup/NicknameInput';
 import PasswordConfirmInput from '@/components/signup/PasswordConfirmInput';
 import PasswordInput from '@/components/signup/PasswordInput';
 import { useWhatTodayStore } from '@/stores';
 
 export default function EditProfilePage() {
+  const navigate = useNavigate();
   const { user } = useWhatTodayStore();
   const [isEditProfileLoading, setIsEditProfileLoading] = useState(false);
   const [profileImage, setProfileImage] = useState<string>(user?.profileImageUrl ?? '');
-  const [email, setEmail] = useState(user?.email ?? '');
   const [nickname, setNickname] = useState(user?.nickname ?? '');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
 
+  const handleBackBtnClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div className='flex flex-col gap-12 text-gray-900'>
-      <h1 className='text-xl font-bold'>내 정보</h1>
+      <div className='flex items-center gap-4'>
+        <Button className='h-fit w-fit' variant='none' onClick={handleBackBtnClick}>
+          <ChevronIcon color='var(--color-gray-300)' direction='left' />
+        </Button>
+        <h1 className='text-xl font-bold'>내 정보</h1>
+      </div>
       <form
         className='flex w-full flex-col items-center justify-center gap-32'
         onReset={() => {
-          setEmail(user?.email ?? '');
           setNickname(user?.nickname ?? '');
           setPassword('');
           setPasswordConfirm('');
@@ -34,10 +42,11 @@ export default function EditProfilePage() {
       >
         <div className='flex w-full flex-col gap-12'>
           <ProfileImageInput src={profileImage} onChange={(value) => setProfileImage(value)} />
-          <EmailInput value={email} onChange={(e) => setEmail(e.target.value)} />
           <NicknameInput value={nickname} onChange={(e) => setNickname(e.target.value)} />
           <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} />
-          <PasswordConfirmInput value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
+          {password.length > 0 && (
+            <PasswordConfirmInput value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} />
+          )}
         </div>
 
         <div className='flex w-full max-w-640 gap-12'>
