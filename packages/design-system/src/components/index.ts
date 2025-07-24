c36085207e1245d65e9683f8ee93e8cb7a324610
MyungJiwoo <1206jiwoo@gmail.com>
2025-07-24 12:26:45 +0900

    🐛 [#112] Fix: 디자인 시스템에서 Popover export

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -11,6 +11,7 @@ export { default as NoResult } from './NoResult';
 export { default as NotificationCard } from './NotificationCard';
 export { default as OwnerBadge } from './OwnerBadge';
 export { default as Pagination } from './Pagination';
+export * from './popover';
 export { default as ProfileImageInput } from './ProfileImageInput';
 export { default as RadioGroup } from './RadioGroup/RadioGroup';
 export { default as ReservationCard } from './ReservationCard';
