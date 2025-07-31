429fdd6a374ec707abc71121e0346d7aa4c94de6
HarrySeop <harryseop@gmail.com>
2025-07-31 13:50:04 +0900

    🎨 [#179] Style: 카드 영역 내 요소 간격 조절

--- packages/design-system/src/components/ReservationCard.tsx
+++ packages/design-system/src/components/ReservationCard.tsx
@@ -85,27 +85,29 @@ export default function ReservationCard({
 
   return (
     <article
-      className='flex h-150 cursor-pointer items-stretch -space-x-38 rounded-2xl border border-gray-50 md:-space-x-20 xl:-space-x-26'
+      className='flex h-150 cursor-pointer items-stretch -space-x-38 rounded-2xl border border-gray-50 md:h-160 md:-space-x-20 xl:-space-x-26'
       onClick={onClick}
     >
-      <section className='z-10 flex w-full flex-col justify-between rounded-l-2xl rounded-r-3xl bg-white p-20'>
-        <header className='flex flex-col xl:gap-4'>
-          <div className='flex flex-col gap-4 xl:gap-6'>
-            {/* badge 컴포넌트로 수정 예정 */}
-            <span className={`caption-text w-fit rounded-full px-8 py-2 text-center font-bold ${style}`}>{badge}</span>
-            <h3 className='section-text'>{title}</h3>
-          </div>
-          <div className='body-text text-gray-400'>
-            <time>{startTime}</time>-<time>{endTime}</time>
-          </div>
-        </header>
-        <div className='body-text flex items-center gap-4'>
+      <section className='z-10 flex w-full flex-col justify-between rounded-l-2xl rounded-r-3xl bg-white p-20 xl:gap-6'>
+        <div className='flex flex-col gap-4 xl:gap-6'>
+          {/* badge 컴포넌트로 수정 예정 */}
+          <span className={`caption-text w-fit rounded-full px-8 py-2 text-center font-bold ${style}`}>{badge}</span>
+          <h3 className='section-text'>{title}</h3>
+        </div>
+        <div className='body-text text-gray-400'>
+          <time>{startTime}</time>-<time>{endTime}</time>
+        </div>
+        <div className='body-text mt-10 flex items-center gap-4 md:mt-0'>
           <div className='font-bold'>₩{formatPrice(totalPrice)}</div>
           <div className='text-gray-400'>{formatPrice(headCount)}명</div>
         </div>
       </section>
       <div className='flex-shrink-0'>
-        <img alt={`${title} 체험 배너`} className='h-full w-150 rounded-r-2xl object-cover' src={bannerImageUrl} />
+        <img
+          alt={`${title} 체험 배너`}
+          className='h-full w-150 rounded-r-2xl object-cover md:w-160'
+          src={bannerImageUrl}
+        />
       </div>
     </article>
   );
