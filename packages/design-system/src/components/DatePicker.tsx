dad0ba527c80d53481d61e28dfc2c291fdfe1117
MyungJiwoo <1206jiwoo@gmail.com>
2025-08-02 16:42:05 +0900

    ♻️ [#212] Refactor: DatePicker에서 날짜를 선택하면 팝오버가 닫히도록 수정

--- packages/design-system/src/components/DatePicker.tsx
+++ packages/design-system/src/components/DatePicker.tsx
@@ -1,4 +1,5 @@
 import dayjs, { type Dayjs } from 'dayjs';
+import { useState } from 'react';
 
 import { Calendar } from './calendar';
 import { CalendarIcon } from './icons';
@@ -12,30 +13,38 @@ interface DatePickerProps {
 }
 
 export default function DatePicker({ value, onChange, disabled }: DatePickerProps) {
+  const [isOpen, setIsOpen] = useState(false);
   const formattedDate = value ? value.format('YY/MM/DD') : '';
   const today = dayjs().format('YYYY-MM-DD');
+
+  const handleDateChange = (newDate: string) => {
+    onChange(dayjs(newDate));
+    setIsOpen(false); // 날짜 선택 시 팝오버 닫기
+  };
 
   return (
     <div className='relative flex flex-col gap-12 md:flex-row'>
       <div className='relative w-full'>
-        <Popover.Root direction='bottom-right' disabled={disabled}>
-          <Popover.Trigger>
-            <Input.Root className='w-full' disabled={disabled}>
-              <Input.Wrapper>
-                <Input.Field readOnly placeholder='년 / 월 / 일' value={formattedDate} />
-                <span className='cursor-pointer'>
-                  <Input.Icon>
-                    <CalendarIcon className='size-20' />
-                  </Input.Icon>
-                </span>
-              </Input.Wrapper>
-              <Input.ErrorMessage />
-            </Input.Root>
+        <Popover.Root direction='bottom-right' disabled={disabled} open={isOpen} onOpenChange={setIsOpen}>
+          <Popover.Trigger asChild>
+            <div className='w-full cursor-pointer' onClick={() => !disabled && setIsOpen(true)}>
+              <Input.Root className='w-full' disabled={disabled}>
+                <Input.Wrapper>
+                  <Input.Field readOnly placeholder='년 / 월 / 일' value={formattedDate} />
+                  <span className='cursor-pointer'>
+                    <Input.Icon>
+                      <CalendarIcon className='size-20' />
+                    </Input.Icon>
+                  </span>
+                </Input.Wrapper>
+                <Input.ErrorMessage />
+              </Input.Root>
+            </div>
           </Popover.Trigger>
 
           <Popover.Content className='date-picker mt-8 flex justify-end'>
             <div className='w-300 rounded-xl border border-gray-100 bg-white p-12'>
-              <Calendar.Root initialDate={today} onDateChange={(newDate) => onChange(dayjs(newDate))}>
+              <Calendar.Root initialDate={today} onDateChange={handleDateChange}>
                 <Calendar.Header headerClass='my-12' />
                 <Calendar.Grid divider weekdayType='short'>
                   {(day) => {
