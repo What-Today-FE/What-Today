4a48e08b3e3efe65ae4ea4e805ebca62cd430c3f
HarrySeop <harryseop@gmail.com>
2025-08-02 17:18:46 +0900

    🔨 [#211] Fix: 오타 수정

--- apps/what-today/src/pages/mypage/reservations-list/index.tsx
+++ apps/what-today/src/pages/mypage/reservations-list/index.tsx
@@ -149,7 +149,7 @@ export default function ReservationsListPage() {
           {group.map((res) => {
             const showCancelButton = res.status === 'pending';
             const showReviewButton = res.status === 'completed' && !res.reviewSubmitted;
-            const shoowReviewCompletedButton = res.status === 'completed' && res.reviewSubmitted;
+            const showReviewCompletedButton = res.status === 'completed' && res.reviewSubmitted;
 
             return (
               <li key={res.id} className='mb-24'>
@@ -164,7 +164,7 @@ export default function ReservationsListPage() {
                   onClick={() => navigate(`/activities/${res.activity.id}`)}
                 />
 
-                {(showCancelButton || showReviewButton || shoowReviewCompletedButton) && (
+                {(showCancelButton || showReviewButton || showReviewCompletedButton) && (
                   <div className='mt-8 mb-24'>
                     {showCancelButton && (
                       <Button
@@ -186,7 +186,7 @@ export default function ReservationsListPage() {
                         후기 작성
                       </Button>
                     )}
-                    {shoowReviewCompletedButton && (
+                    {showReviewCompletedButton && (
                       <Button disabled className='caption-text w-full' size='md' variant='fill'>
                         후기 작성 완료
                       </Button>
