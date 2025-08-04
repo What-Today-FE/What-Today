270c906af5c71aa1b988e6d1013178c04137b628
Taeil08 <ejrrhcl56@naver.com>
2025-08-05 01:38:17 +0900

    ✨[#265] Feat: 배너 이미지 수정

--- packages/design-system/src/components/MainBanner/dummyBanners.ts
+++ packages/design-system/src/components/MainBanner/dummyBanners.ts
@@ -7,32 +7,32 @@ export interface BannerItem {
 export const dummyBanners: BannerItem[] = [
   {
     id: 1,
-    title: '햇살 가득한 해변에서의 휴식',
-    imageUrl: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=1600&q=80',
+    title: '한옥 찜질방과 전통 음식 체험',
+    imageUrl:
+      'https://media.discordapp.net/attachments/1390185747964887212/1401964797418803443/553482876fe1285e.webp?ex=689230f6&is=6890df76&hm=fbfadcc138d023bccd1f96ce2d531d221945e36cfa9047f5faa9367019ea9cb9&=&format=webp&width=1888&height=1258',
   },
   {
     id: 2,
-    title: '설산을 품은 힐링 트레킹',
-    imageUrl: 'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1600&q=80',
+    title: '경복궁 관광과 한복 체험',
+    imageUrl:
+      'https://media.discordapp.net/attachments/1390185747964887212/1401964798035493055/image_1.webp?ex=689230f7&is=6890df77&hm=fa3437927f56460d00687f52eb2feb58e9adc2d6a19c944fb106df168af2f688&=&format=webp&width=1888&height=1258',
   },
   {
     id: 3,
-    title: '야경이 아름다운 도시 여행',
-    imageUrl: 'https://images.unsplash.com/photo-1508057198894-247b23fe5ade?auto=format&fit=crop&w=1600&q=80',
+    title: '제주도 해녀와 해산물 체험',
+    imageUrl:
+      'https://media.discordapp.net/attachments/1390185747964887212/1401964798605922458/image.webp?ex=689230f7&is=6890df77&hm=cea5d79844009d43cdb654776b858545551f0bc1120e0e42fa8a8b2024660705&=&format=webp&width=1888&height=1258',
   },
   {
     id: 4,
-    title: '이국적인 사막에서의 낙타 체험',
-    imageUrl: 'https://images.unsplash.com/photo-1541417904950-b855846fe074?auto=format&fit=crop&w=1600&q=80',
+    title: '남산타워와 서울 야경 관광',
+    imageUrl:
+      'https://media.discordapp.net/attachments/1390185747964887212/1401964799163629719/raw_1.webp?ex=689230f7&is=6890df77&hm=f2cf0af0e1276a7afb1d637f4e990c56e75317e49d7fb64805d84476ba8b2a76&=&format=webp&width=1888&height=1258',
   },
   {
     id: 5,
-    title: '감성 가득 유럽 골목 여행',
-    imageUrl: 'https://images.unsplash.com/photo-1493558103817-58b2924bce98?auto=format&fit=crop&w=1600&q=80',
-  },
-  {
-    id: 6,
-    title: '고요한 숲 속의 명상 캠프',
-    imageUrl: 'https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1600&q=80',
+    title: '경주 첨성대 야경 관광',
+    imageUrl:
+      'https://media.discordapp.net/attachments/1390185747964887212/1401942617784057866/image.png?ex=68921c4e&is=6890cace&hm=89563fc267fa032346bc49ad4fae8c769b0431529aa9814a229ad7bdccc2c9bc&=&format=webp&quality=lossless&width=1901&height=1267',
   },
 ];
