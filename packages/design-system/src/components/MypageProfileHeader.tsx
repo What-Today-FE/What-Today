59eba86c20d5e3e4bc534513633a366d4c3d9b9f
MyungJiwoo <1206jiwoo@gmail.com>
2025-08-04 22:34:22 +0900

    ♻️ [#254] Refactor: 마이페이지 프로필 위젯이 잘리지 않게 리팩토링

--- packages/design-system/src/components/MypageProfileHeader.tsx
+++ packages/design-system/src/components/MypageProfileHeader.tsx
@@ -12,11 +12,11 @@ interface MypageProfileHeaderProps {
 export default function MypageProfileHeader({ name, email, profileImageUrl, onLogoutClick }: MypageProfileHeaderProps) {
   return (
     <div className='flex items-center gap-36 rounded-3xl border border-gray-50 bg-white px-36 py-36'>
-      <div className='flex size-120 items-center justify-center rounded-full border border-gray-50 bg-white'>
+      <div className='flex aspect-square size-120 items-center justify-center rounded-full border border-gray-50 bg-white'>
         {profileImageUrl ? (
           <img
             alt='프로필 이미지'
-            className='size-110 rounded-full border border-gray-50 bg-white object-cover'
+            className='aspect-square size-110 rounded-full border border-gray-50 bg-white object-cover'
             src={profileImageUrl}
           />
         ) : (
@@ -26,8 +26,8 @@ export default function MypageProfileHeader({ name, email, profileImageUrl, onLo
 
       <div className='flex h-80 flex-col justify-center gap-16 text-gray-950'>
         <div className='flex flex-col'>
-          <p className='title-text font-bold'>{name}</p>
-          <p className='body-text text-gray-400'>{email}</p>
+          <p className='title-text line-clamp-1 font-bold'>{name}</p>
+          <p className='body-text line-clamp-1 text-gray-400'>{email}</p>
         </div>
         <Button
           className='caption-text h-auto w-auto justify-start p-0 text-gray-400'
