8c475a97f3eb6b3c974ed85d279ed8f78ca24e80
Taeil08 <ejrrhcl56@naver.com>
2025-08-05 01:55:18 +0900

    ♻️[#265] Refactor: 배너 url 수정

--- packages/design-system/src/components/MainBanner/dummyBanners.ts
+++ packages/design-system/src/components/MainBanner/dummyBanners.ts
@@ -9,30 +9,30 @@ export const dummyBanners: BannerItem[] = [
     id: 1,
     title: '한옥 찜질방과 전통 음식 체험',
     imageUrl:
-      'https://media.discordapp.net/attachments/1390185747964887212/1401964797418803443/553482876fe1285e.webp?ex=689230f6&is=6890df76&hm=fbfadcc138d023bccd1f96ce2d531d221945e36cfa9047f5faa9367019ea9cb9&=&format=webp&width=1888&height=1258',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/15-4_2282_1754326460744.webp',
   },
   {
     id: 2,
     title: '경복궁 관광과 한복 체험',
     imageUrl:
-      'https://media.discordapp.net/attachments/1390185747964887212/1401964798035493055/image_1.webp?ex=689230f7&is=6890df77&hm=fa3437927f56460d00687f52eb2feb58e9adc2d6a19c944fb106df168af2f688&=&format=webp&width=1888&height=1258',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/15-4_2282_1754326393840.webp',
   },
   {
     id: 3,
     title: '제주도 해녀와 해산물 체험',
     imageUrl:
-      'https://media.discordapp.net/attachments/1390185747964887212/1401964798605922458/image.webp?ex=689230f7&is=6890df77&hm=cea5d79844009d43cdb654776b858545551f0bc1120e0e42fa8a8b2024660705&=&format=webp&width=1888&height=1258',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/15-4_2282_1754326351593.webp',
   },
   {
     id: 4,
     title: '남산타워와 서울 야경 관광',
     imageUrl:
-      'https://media.discordapp.net/attachments/1390185747964887212/1401964799163629719/raw_1.webp?ex=689230f7&is=6890df77&hm=f2cf0af0e1276a7afb1d637f4e990c56e75317e49d7fb64805d84476ba8b2a76&=&format=webp&width=1888&height=1258',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/15-4_2282_1754326320772.webp',
   },
   {
     id: 5,
     title: '경주 첨성대 야경 관광',
     imageUrl:
-      'https://media.discordapp.net/attachments/1390185747964887212/1401942617784057866/image.png?ex=68921c4e&is=6890cace&hm=89563fc267fa032346bc49ad4fae8c769b0431529aa9814a229ad7bdccc2c9bc&=&format=webp&quality=lossless&width=1901&height=1267',
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/15-4_2282_1754326417757.webp',
   },
 ];
