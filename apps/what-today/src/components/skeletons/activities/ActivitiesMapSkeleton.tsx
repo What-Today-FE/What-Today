63b4a07b679c652b9c86115c15f7e77c6e5f5be2
HarrySeop <harryseop@gmail.com>
2025-08-05 11:29:15 +0900

    🎨 [#277] Style: 스켈레톤 색상 통일

--- apps/what-today/src/components/skeletons/activities/ActivitiesMapSkeleton.tsx
+++ apps/what-today/src/components/skeletons/activities/ActivitiesMapSkeleton.tsx
@@ -2,14 +2,14 @@ export default function ActivitiesMapSkeleton() {
   return (
     <section className='section-text flex h-511 w-full flex-col justify-start gap-8'>
       {/* 섹션 제목 */}
-      <div className='h-20 w-80 animate-pulse rounded bg-gray-200' />
+      <div className='h-20 w-80 animate-pulse rounded bg-gray-100' />
 
       {/* 주소 */}
-      <div className='h-16 w-200 animate-pulse rounded bg-gray-200' />
+      <div className='h-16 w-200 animate-pulse rounded bg-gray-100' />
 
       {/* 지도 영역 */}
       <div className='h-full w-full overflow-hidden rounded-3xl'>
-        <div className='h-full w-full animate-pulse bg-gray-200' />
+        <div className='h-full w-full animate-pulse bg-gray-100' />
       </div>
     </section>
   );
